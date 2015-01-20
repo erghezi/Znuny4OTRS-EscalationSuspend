bf5dc43bc3d15f400711bf927b02118494c17f6a
erghezi <erghezi@gmail.com>
2015-01-21 01:58:29 +0330

    Ported to OTRS 4

--- bin/znuny.RebuildEscalationIndexOnline.pl
+++ bin/znuny.RebuildEscalationIndexOnline.pl
@@ -12,15 +12,7 @@ use File::Basename;
 use FindBin qw($RealBin);
 use lib dirname($RealBin);
 use lib dirname($RealBin) . "/Kernel/cpan-lib";
-
 use Getopt::Std;
-use Kernel::Config;
-use Kernel::System::Log;
-use Kernel::System::Time;
-use Kernel::System::Encode;
-use Kernel::System::DB;
-use Kernel::System::Main;
-use Kernel::System::Ticket;
 
 # get options
 my %Opts = ();
@@ -32,31 +24,33 @@ if ( $Opts{h} ) {
     exit 1;
 }
 
-# create common objects
-my %CommonObject = ();
-$CommonObject{ConfigObject} = Kernel::Config->new();
-$CommonObject{LogObject}    = Kernel::System::Log->new(
-    LogPrefix => 'OTRS-znuny.RebuildEscalationIndexOnline',
-    %CommonObject,
-);
-$CommonObject{MainObject}   = Kernel::System::Main->new(%CommonObject);
-$CommonObject{EncodeObject} = Kernel::System::Encode->new(%CommonObject);
-$CommonObject{TimeObject}   = Kernel::System::Time->new( %CommonObject, );
+use Kernel::System::ObjectManager;
 
-# create needed objects
-$CommonObject{DBObject}     = Kernel::System::DB->new(%CommonObject);
-$CommonObject{TicketObject} = Kernel::System::Ticket->new(%CommonObject);
+# create common objects
+local $Kernel::OM = Kernel::System::ObjectManager->new(
+    'Kernel::System::Log' => {
+        LogPrefix => 'OTRS-znuny.RebuildEscalationIndexOnline',
+    },
+);
+
+# get database object
+my $DBObject = $Kernel::OM->Get('Kernel::System::DB');
+my $TimeObject = $Kernel::OM->Get('Kernel::System::Time');
+my $ConfigObject = $Kernel::OM->Get('Kernel::Config');
+my $MainObject   = $Kernel::OM->Get('Kernel::System::Main');
+
 
 # get all tickets
-my @TicketIDs = $CommonObject{TicketObject}->TicketSearch(
+my @TicketIDs = $Kernel::OM->Get('Kernel::System::Ticket')->TicketSearch(
 
     # result (required)
     Result => 'ARRAY',
 
-    States => $CommonObject{ConfigObject}->Get('EscalationSuspendStates'),
+    States => $ConfigObject->Get('EscalationSuspendStates'),
 
     # result limit
-    Limit      => 100_000_000,
+    #Limit      => 100_000_000,
+    Limit      => 1000,
     UserID     => 1,
     Permission => 'ro',
 );
@@ -64,10 +58,11 @@ my @TicketIDs = $CommonObject{TicketObject}->TicketSearch(
 my $Count = 0;
 for my $TicketID (@TicketIDs) {
     $Count++;
-    $CommonObject{TicketObject}->TicketEscalationIndexBuild(
+    $Kernel::OM->Get('Kernel::System::Ticket')->TicketEscalationIndexBuild(
         TicketID => $TicketID,
         UserID   => 1,
     );
+ 
     if ( ( $Count / 2000 ) == int( $Count / 2000 ) ) {
         my $Percent = int( $Count / ( $#TicketIDs / 100 ) );
         print "NOTICE: $Count of $#TicketIDs processed ($Percent% done).\n";
